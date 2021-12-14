11ed8d46bd49c75a1e34e92098fece346b72ca7d
vmartin <vmartin@gc.axesor.com>
2021-12-14 17:46:52 +0100

    ChangingActorBehaviorMain StatelessFussyKid/Mom

---
+++ src/main/scala/com/example/part2actors/ChangingActorBehavior.scala
@@ -0,0 +1,81 @@
+package com.example.part2actors
+
+import akka.actor.typed.{ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorRef
+
+object StatelessFussyKid {
+  import Mom._
+
+  val VEGETABLE = "veggies"
+  val CHOCOLATE = "chocolate"
+
+  sealed trait Command
+  final case class Food(food: String) extends Command
+  final case class Ask(message: String, replyTo: ActorRef[MomMessage]) extends Command // do you want to play?
+
+  def apply(): Behavior[Command] = happyReceive()
+
+  private def happyReceive(): Behavior[Command] =
+    Behaviors.receiveMessage {
+      case Food(VEGETABLE) => sadReceive()
+      case Food(CHOCOLATE) => Behaviors.same
+      case Ask(_, replyTo) =>
+        replyTo ! KidAccept
+        Behaviors.same
+    }
+
+  private def sadReceive(): Behavior[Command] =
+    Behaviors.receiveMessage {
+      case Food(VEGETABLE) => Behaviors.same
+      case Food(CHOCOLATE) => happyReceive()
+      case Ask(_, replyTo) =>
+        replyTo ! KidReject
+        Behaviors.same
+    }
+}
+
+object Mom {
+  import StatelessFussyKid._
+  sealed trait MomMessage
+  final case class MomStart(kidRef: ActorRef[Command]) extends MomMessage
+  case object KidAccept extends MomMessage
+  case object KidReject extends MomMessage
+
+  def apply(): Behavior[MomMessage] = Behaviors.receive { (actorContext, message) =>
+    message match {
+      case MomStart(kidRef) =>
+        kidRef ! Ask("do you want to play?", actorContext.self)
+        kidRef ! Food(VEGETABLE)
+        kidRef ! Ask("do you want to play?", actorContext.self)
+        Behaviors.same
+      case KidAccept =>
+        println("Yay, my kid is happy!")
+        Behaviors.same
+      case KidReject =>
+        println("My kid is sad, but as he's healthy!")
+        Behaviors.same
+    }
+  }
+}
+
+object ChangingActorBehaviorMain {
+  final case class SayHello()
+
+  def apply(): Behavior[SayHello] =
+    Behaviors.setup { context =>
+      val statelessFussyKid = context.spawn(StatelessFussyKid(), "statelessFussyKid")
+      val mom = context.spawn(Mom(), "mom")
+
+      Behaviors.receiveMessage { _ =>
+        mom ! Mom.MomStart(statelessFussyKid)
+        Behaviors.same
+      }
+    }
+}
+
+object ChangingActorBehavior extends App {
+  val changingActorBehaviorMain : ActorSystem[ChangingActorBehaviorMain.SayHello] = ActorSystem(ChangingActorBehaviorMain(), "ChangingActorBehavior")
+
+  changingActorBehaviorMain ! ChangingActorBehaviorMain.SayHello()
+}
