85eba32661f194d042aaae4a68fcb3528a60c3f5
vmartin <vmartin@gc.axesor.com>
2022-01-05 13:39:38 +0100

    TestProbeSpec: send the work to the slave actor

--- src/main/scala/com/example/part3testing/basicspec/TestProbeSpec.scala
+++ src/main/scala/com/example/part3testing/basicspec/TestProbeSpec.scala
@@ -7,17 +7,19 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-final case class RegistrationAck()
+final case class SlaveWork(text: String, originalRequester: ActorRef[Report])
 
-final case class SlaveWork(text: String, originalRequester: ActorRef[_])
+sealed trait SenderMessages
 
-case class Report(totalCount: Int)
+final case class RegistrationAck() extends SenderMessages
+
+case class Report(totalCount: Int) extends SenderMessages
 
 sealed trait MasterMessages
 
 final case class Register(sender: ActorRef[RegistrationAck], slaveRef: ActorRef[SlaveWork]) extends MasterMessages
 
-final case class Work(sender: ActorRef[RegistrationAck], text: String) extends MasterMessages
+final case class Work(sender: ActorRef[Report], text: String) extends MasterMessages
 
 final case class WorkCompleted(count: Int, originalRequester: ActorRef[Report]) extends MasterMessages
 
@@ -33,11 +35,31 @@ class TestProbeSpec
   "A master actor" should {
     "register a slave" in {
       val master = testKit.spawn(Master(), "master")
-      val sender = testKit.createTestProbe[RegistrationAck]()
+      val sender = testKit.createTestProbe[SenderMessages]()
       val slave = testKit.createTestProbe[SlaveWork]()
 
       master ! Register(sender.ref, slave.ref)
+
       sender.receiveMessage()
+    }
+
+    "send the work to the slave actor" in {
+      val master = testKit.spawn(Master(), "master2")
+      val sender = testKit.createTestProbe[SenderMessages]()
+      val slave = testKit.createTestProbe[SlaveWork]()
+      val mockedBehavior = Behaviors.receiveMessage[SlaveWork] { msg =>
+        msg.originalRequester ! Report(3)
+        Behaviors.same
+      }
+      val mockedSlave = testKit.spawn(Behaviors.monitor(slave.ref, mockedBehavior))
+
+      master ! Register(sender.ref, mockedSlave)
+      val workloadString = "I love Akka"
+      master ! Work(sender.ref, workloadString)
+
+      sender.receiveMessage()
+      slave.expectMessage(SlaveWork(workloadString, sender.ref))
+      sender.expectMessage(Report(3))
     }
   }
 }
